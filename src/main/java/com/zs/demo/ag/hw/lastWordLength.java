2a62a7c9762fc55a081ec42941f77d6c066c544d
Deserialization <m18092971720@163.com>
2021-02-18 16:36:13 +0800

    add ag

--- src/main/java/com/zs/demo/ag/hw/lastWordLength.java
+++ src/main/java/com/zs/demo/ag/hw/lastWordLength.java
@@ -5,19 +5,91 @@ import java.util.Scanner;
 public class lastWordLength {
 
 
-    /**
-     * 字符串最后一个单词的长度
-     * @param args
-     */
     public static void main(String[] args) {
 
+
+        // /addAndSub();
+        //divNumber();
+        //transNum();
+        //splitString();
+        //lastWordLength();
+    }
+
+    private static void addAndSub() {
+        /**
+         * 写出一个程序，接受一个正浮点数值，输出该数值的近似整数值。
+         * 如果小数点后数值大于等于5,向上取整；小于5，则向下取整。
+         */
+        Scanner scanner = new Scanner(System.in);
+        double number = scanner.nextDouble();
+        int i = (int)(number);
+        System.out.println((number-i)>=0.5?i+1:i);
+    }
+
+    /**
+     * 输入一个正整数，按照从小到大的顺序输出它的所有质因子（重复的也要列举）
+     * （如180的质因子为2 2 3 3 5 ）
+     */
+    private static void divNumber() {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sbBuilder = new StringBuilder();
+
+        long num = scanner.nextLong();
+
+        long t = num;
+        for (int i=2;i<=t; i++) {
+            while (num % i == 0) {
+                num = num/i;
+                sbBuilder.append(i);
+                sbBuilder.append(" ");
+            }
+        }
+        System.out.println(sbBuilder.toString());
+    }
+
+    /**
+     * 16进制转10进制
+     */
+    private static void transNum() {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str = sc.next().substring(2);
+            System.out.println(Integer.parseInt(str, 16));
+        }
+    }
+
+    private static void splitString() {
+        /**
+         * •连续输入字符串，请按长度为8拆分每个字符串后输出到新的字符串数组；
+         •长度不是8整数倍的字符串请在后面补数字0，空字符串不处理。
+         */
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String st = scanner.nextLine();
+            while (st.length() >= 8) {
+                System.out.println(st.substring(0, 8));
+                st = st.substring(8);
+            }
+            if (st.length() < 8 && st.length() > 0) {
+                st = st + "00000000";
+                System.out.println(st.substring(0, 8));
+            }
+
+            System.out.println(st);
+        }
+    }
+
+
+    /**
+     * 字符串最后一个单词的长度s
+     */
+    private static void lastWordLength() {
         Scanner scanner = new Scanner(System.in);
         String st = scanner.nextLine();
         String[] arr = st.split("\\s+");
 
         int n = arr[arr.length - 1].length();
         System.out.println(n);
-
     }
 }
 
